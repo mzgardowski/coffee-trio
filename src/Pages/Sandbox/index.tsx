d63f580c59429085b34987d81419f94fb68ae670
Mateusz Zgardowski <m.zgardowski@global4net.com>
2023-03-22 20:04:38 +0100

    main/ts config

--- src/Pages/Sandbox/index.tsx
+++ src/Pages/Sandbox/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { colors } from 'coffeeTrio/src/assets/colors';
+import { colors } from 'src/assets/colors';
 
 const Sandbox = () => {
   return (
