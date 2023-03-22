d1340913be2f6dcd18e0560ef89c8e0466eb3a7f
Mateusz Zgardowski <m.zgardowski@global4net.com>
2023-03-22 21:28:36 +0100

    main/ prepare fonts, colors

--- App.tsx
+++ App.tsx
@@ -1,10 +1,3 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 import { SafeAreaView, ScrollView } from 'react-native';
 import SandBox from './src/Pages/Sandbox';
@@ -13,7 +6,6 @@ function App(): JSX.Element {
   return (
     <SafeAreaView>
       <ScrollView contentInsetAdjustmentBehavior="automatic">
-        {/* <Text>My App name is {APP_NAME}</Text>s */}
         <SandBox />
       </ScrollView>
     </SafeAreaView>
--- src/Pages/Sandbox/index.tsx
+++ src/Pages/Sandbox/index.tsx
@@ -1,18 +1,104 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
 import { colors } from 'src/assets/colors';
 
 const Sandbox = () => {
   return (
     <View>
-      <View style={styles.background} />
+      <Text style={styles.textStyle}>Hello World</Text>
+      <Text style={styles.textStyleDancingMedium}>Hello World</Text>
+      <Text style={styles.textStyleDancingRegular}>Hello World</Text>
+      <Text style={styles.textStyleInconsolataBold}>Hello World</Text>
+      <Text style={styles.textStyleInconsolataRegular}>Hello World</Text>
+      <Text style={styles.textStyleInconsolataLight}>Hello World</Text>
+      <View style={[styles.background, styles.blockSize]} />
+      <View style={[styles.secondarybBackground, styles.blockSize]} />
+      <View style={[styles.primaryButton, styles.blockSize]} />
+      <View style={[styles.greenBtn, styles.blockSize]} />
+      <View style={[styles.redBtn, styles.blockSize]} />
+      <View style={[styles.textPrimary, styles.blockSize]} />
+      <View style={[styles.borderPrimary, styles.blockSize]} />
+      <View style={[styles.borderSecondary, styles.blockSize]} />
+      <View style={[styles.borderRed, styles.blockSize]} />
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  blockSize: {
+    width: 120,
+    height: 60,
+    borderRadius: 8,
+    margin: 8,
+  },
   background: {
     backgroundColor: colors.primary_background,
+  },
+  secondarybBackground: {
+    backgroundColor: colors.secondary_bacground,
+  },
+  primaryButton: {
+    backgroundColor: colors.primary_button,
+  },
+  greenBtn: {
+    backgroundColor: colors.button_green,
+  },
+  redBtn: {
+    backgroundColor: colors.button_red,
+  },
+  textPrimary: {
+    backgroundColor: colors.text_primary,
+  },
+  textSecondary: {
+    backgroundColor: colors.text_secondary,
+  },
+  textBlack: {
+    backgroundColor: colors.text_black,
+  },
+  textGray: {
+    backgroundColor: colors.text_gray,
+  },
+  textWhite: {
+    backgroundColor: colors.text_white,
+  },
+  textRed: {
+    backgroundColor: colors.text_red,
+  },
+  borderPrimary: {
+    borderColor: colors.primary_border,
+    borderWidth: 4,
+  },
+  borderSecondary: {
+    borderColor: colors.secondary_border,
+    borderWidth: 4,
+  },
+  borderRed: {
+    borderColor: colors.red_border,
+    borderWidth: 4,
+  },
+  textStyle: {
+    fontFamily: 'DancingScript-Bold',
+    fontSize: 38,
+  },
+  textStyleDancingMedium: {
+    fontFamily: 'DancingScript-Medium',
+    fontSize: 30,
+  },
+  textStyleDancingRegular: {
+    fontFamily: 'DancingScript-Regular',
+    fontSize: 22,
+  },
+  textStyleInconsolataBold: {
+    fontFamily: 'Inconsolata-Bold',
+    fontSize: 38,
+  },
+  textStyleInconsolataRegular: {
+    fontFamily: 'Inconsolata-Regular',
+    fontSize: 30,
+  },
+  textStyleInconsolataLight: {
+    fontFamily: 'Inconsolata-Light',
+    fontSize: 22,
   },
 });
 
